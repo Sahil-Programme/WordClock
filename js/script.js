8d37041d2a8731c9a4b1d57fa7f3f794d3b4bc9e
Sahil-Programme <sahilyadav127@gmail.com>
2021-08-08 22:56:28 +1000

    matrix and title added

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var wordMatrix = 
+["HHAPPYLBIRTHDAYT"
+,"FJKXCHARLYUIWZTV"
+,"ITVISYTWENTYHALF"
+,"ONELEVENINETEENC"
+,"TWELVESIXTEENTEN"
+,"THIRTEENEIGHTEEN"
+,"FOURTEENTWOVFIVE"
+,"QUARTERSEVENTEEN"
+,"THREEHPASTOUFIVE"
+,"FOURTWELVEIGHTEN"
+,"NINEFIVESIXTHREE"
+,"TWONELEVENSEVENT"
+,"OCLOCKFINATHESTX"
+,"EVENINGAFTERNOON"
+,"MORNINGNIGHTIAND"
+,"COOLCOLDWARMHOTV"];
+
+//var element = document.createElement("div");
+//    element.appendChild(document.createElement() .createTextNode('The man who mistook his wife for a hat'));
+//    document.getElementById('matrix').appendChild(element);
+
+
+//<h1><span>WORD</span><span>WORD</span></h1>
+var str = "<div><h1 class=\"inmatrixComponent\">";
+for(var x = 0;x < 16; x++){
+    for(var y = 0;y < 16; y++){
+        str += '<span class="block">'+wordMatrix[x][y]+'</span>';        
+    }
+    str += '<br>';
+}
+str += '</h1></div>'
+
+var div = document.getElementById('matrix');
+div.innerHTML += str;
+
+    
+
+//var elem = document.getElementsByClassName('matrix');
+//elem.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100';
+//document.body.appendChild(elem);
