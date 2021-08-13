e3f18db13f540b46086057fa8136987bdf5591cd
Sahil-Programme <sahilyadav127@gmail.com>
2021-08-14 04:38:45 +1000

    new

--- script/script.js
+++ script/script.js
@@ -1,14 +1,5 @@
-let currentIndex = 0;
-const container = document.getElementById('card-Container');
-
 window.setInterval(update, 4000);
 
 function update() {
-    currentIndex++;
-    if (currentIndex == 4) {
-        currentIndex = 0;
-    }
-    let x = -25 * currentIndex;
 
-    container.style.left = `${x}em`;
 }
