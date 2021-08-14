d9d38315e3d19b692561345add45067e5d305528
Sahil-Programme <sahilyadav127@gmail.com>
2021-08-14 23:12:26 +1000

    added carousel sliding function

--- script/script.js
+++ script/script.js
@@ -1,5 +1,32 @@
-window.setInterval(update, 4000);
+let currentIndex = 0;
+
+const img1 = document.getElementById('image-1');
+const img2 = document.getElementById('image-2');
+const img3 = document.getElementById('image-3');
+
+window.setInterval(update, 1000);
 
 function update() {
+    currentIndex++;
+    if (currentIndex == 3) {
+        currentIndex = 0;
+    }
+
+    if (currentIndex == 1) {
+        img1.style.left = `20%`;
+        img2.style.left = `50%`;
+        img3.style.left = `80%`;
+    } else if (currentIndex == 2) {
+        img1.style.left = `80%`;
+        img2.style.left = `20%`;
+        img3.style.left = `50%`;
+    } else {
+        img1.style.left = `50%`;
+        img2.style.left = `80%`;
+        img3.style.left = `20%`;
+    }
+
+
+
 
 }
