bbed4ec10a5f7dad4afc27a5bdd3c9d98aa7db58
Sahil-Programme <sahilyadav127@gmail.com>
2021-08-15 00:26:21 +1000

    jsCarousel

--- script/script.js
+++ script/script.js
@@ -1,41 +1,13 @@
 let currentIndex = 0;
 
-const img1 = document.getElementById('image-1');
-const img2 = document.getElementById('image-2');
-const img3 = document.getElementById('image-3');
+const img1 = document.getElementById('carousel-container');
 
-window.setInterval(update, 2000);
+init_carousel();
+
+function init_carousel() {
+
+}
 
 function update() {
-
-    var styles = '';
-    styles += `.r0c0 {
-        color: rgb(80, 80, 255);
-      }`
-    var styleSheet = document.createElement("style")
-    styleSheet.innerText += styles
-    document.head.appendChild(styleSheet)
-
-    currentIndex++;
-    if (currentIndex == 3) {
-        currentIndex = 0;
-    }
-
-    if (currentIndex == 1) {
-        img1.style.left = `20%`;
-        img2.style.left = `50%`;
-        img3.style.left = `80%`;
-    } else if (currentIndex == 2) {
-        img1.style.left = `80%`;
-        img2.style.left = `20%`;
-        img3.style.left = `50%`;
-    } else {
-        img1.style.left = `50%`;
-        img2.style.left = `80%`;
-        img3.style.left = `20%`;
-    }
-
-
-
 
 }
