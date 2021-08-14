378fd8c8288273f8c16b344606084795ba677925
Sahil-Programme <sahilyadav127@gmail.com>
2021-08-15 03:32:40 +1000

    Adding classes using JS

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
-let currentIndex = 0;
+let currentIndex = -1;
 
 let img1 = document.getElementById("img1");
 let img2 = document.getElementById("img2");
 let img3 = document.getElementById("img3");
+
+let parent_div_1 = document.getElementById("parent1");
+let parent_div_2 = document.getElementById("parent2");
+let parent_div_3 = document.getElementById("parent3");
+
 
 const imageSource1 = '../images/no bg/1NoBG.png';
 const imageSource2 = '../images/no bg/2NoBG.png';
@@ -13,6 +18,15 @@ window.setInterval(update, 1000);
 init_carousel();
 
 function init_carousel() {
+
+    parent_div_1.classList.add("image-1");
+    parent_div_2.classList.add("image-2");
+    parent_div_3.classList.add("image-3");
+
+    img1.classList.add("side-crop");
+    img2.classList.add("center-crop");
+    img3.classList.add("side-crop");
+
     img1.src = imageSource1;
     img2.src = imageSource2;
     img3.src = imageSource3;
@@ -20,21 +34,17 @@ function init_carousel() {
 }
 
 function update() {
+
+    var class1 = document.getElementsByClassName('col1');
     if (currentIndex == 3)
         currentIndex = 0;
-
     if (currentIndex == 0) {
-        img1.src = imageSource1;
-        img2.src = imageSource2;
-        img3.src = imageSource3;
+        parent_div_1.image
+            //
     } else if (currentIndex == 1) {
-        img1.src = imageSource3;
-        img2.src = imageSource1;
-        img3.src = imageSource2;
+        //
     } else {
-        img1.src = imageSource2;
-        img2.src = imageSource3;
-        img3.src = imageSource1;
+        //
     }
     currentIndex++;
 }
