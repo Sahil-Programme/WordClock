c34cff3a8bbe37c65ad253b0e0e8cbeaf1bd0759
Sahil-Programme <sahilyadav127@gmail.com>
2021-08-14 23:59:05 +1000

    refine

--- script/script.js
+++ script/script.js
@@ -4,9 +4,18 @@ const img1 = document.getElementById('image-1');
 const img2 = document.getElementById('image-2');
 const img3 = document.getElementById('image-3');
 
-window.setInterval(update, 1000);
+window.setInterval(update, 2000);
 
 function update() {
+
+    var styles = '';
+    styles += `.r0c0 {
+        color: rgb(80, 80, 255);
+      }`
+    var styleSheet = document.createElement("style")
+    styleSheet.innerText += styles
+    document.head.appendChild(styleSheet)
+
     currentIndex++;
     if (currentIndex == 3) {
         currentIndex = 0;
