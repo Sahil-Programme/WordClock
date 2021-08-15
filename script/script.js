817c1bb11fa60b9f0e76c78014044a704c736461
Sahil-Programme <sahilyadav127@gmail.com>
2021-08-15 20:52:17 +1000

    final carousel with three images

--- script/script.js
+++ script/script.js
@@ -3,48 +3,69 @@ let currentIndex = -1;
 let img1 = document.getElementById("img1");
 let img2 = document.getElementById("img2");
 let img3 = document.getElementById("img3");
+let img4 = document.getElementById("img4");
+let img5 = document.getElementById("img5");
 
-let parent_div_1 = document.getElementById("parent1");
-let parent_div_2 = document.getElementById("parent2");
-let parent_div_3 = document.getElementById("parent3");
-
-
-const imageSource1 = '../images/no bg/1NoBG.png';
-const imageSource2 = '../images/no bg/2NoBG.png';
-const imageSource3 = '../images/no bg/3NoBG.png';
-
-window.setInterval(update, 1000);
+window.setInterval(update, 3000);
 
 init_carousel();
 
 function init_carousel() {
+    img1.style.left = "20%";
+    img2.style.left = "50%";
+    img3.style.left = "80%";
 
-    parent_div_1.classList.add("image-1");
-    parent_div_2.classList.add("image-2");
-    parent_div_3.classList.add("image-3");
+    img1.style.zIndex = 50;
+    img2.style.zIndex = 100;
+    img3.style.zIndex = 75;
 
-    img1.classList.add("side-crop");
-    img2.classList.add("center-crop");
-    img3.classList.add("side-crop");
+    img2.style.width = "375px";
 
-    img1.src = imageSource1;
-    img2.src = imageSource2;
-    img3.src = imageSource3;
+    img4.style.left = "120%";
+    img5.style.left = "1500%";
     currentIndex++;
 }
 
 function update() {
 
-    var class1 = document.getElementsByClassName('col1');
     if (currentIndex == 3)
         currentIndex = 0;
-    if (currentIndex == 0) {
-        parent_div_1.image
-            //
-    } else if (currentIndex == 1) {
-        //
+    if (currentIndex == 1) {
+        img1.style.left = "80%";
+        img2.style.left = "20%";
+        img3.style.left = "50%";
+
+        img3.style.width = "375px";
+        img2.style.width = "275px";
+        img1.style.width = "275px";
+
+        img1.style.zIndex = 50;
+        img2.style.zIndex = 75;
+        img3.style.zIndex = 100;
+    } else if (currentIndex == 2) {
+        img1.style.left = "50%";
+        img2.style.left = "80%";
+        img3.style.left = "20%";
+
+        img1.style.width = "375px";
+        img3.style.width = "275px";
+        img2.style.width = "275px";
+
+        img1.style.zIndex = 100;
+        img2.style.zIndex = 50;
+        img3.style.zIndex = 75;
     } else {
-        //
+        img1.style.left = "20%";
+        img2.style.left = "50%";
+        img3.style.left = "80%";
+
+        img2.style.width = "375px";
+        img3.style.width = "275px";
+        img1.style.width = "275px";
+
+        img1.style.zIndex = 75;
+        img2.style.zIndex = 100;
+        img3.style.zIndex = 50;
     }
     currentIndex++;
 }
