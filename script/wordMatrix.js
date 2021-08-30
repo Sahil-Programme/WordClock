13bae0c43c89360447fba562d9b1cfc40ed94bd8
Sahil-Programme <sahilyadav127@gmail.com>
2021-08-31 00:52:30 +1000

    got a static matrix ->  make the matrix resisable next

--- script/wordMatrix.js
+++ script/wordMatrix.js
@@ -1,2 +1,48 @@
-const ul = document.getElementById('matrix');
-//const charlength;
+var wordMatrix = ["HHAPPYLBIRTHDAYT",
+    "CFJKXCHARLYUIWZT",
+    "ITVISYTWENTYHALF",
+    "ONELEVENINETEENC",
+    "TWELVESIXTEENTEN",
+    "THIRTEENEIGHTEEN",
+    "FOURTEENTWOVFIVE",
+    "QUARTERSEVENTEEN",
+    "THREEHPASTOUFIVE",
+    "FOURTWELVEIGHTEN",
+    "NINEFIVESIXTHREE",
+    "TWONELEVENSEVENT",
+    "OCLOCKFINATHESTX",
+    "EVENINGAFTERNOON",
+    "MORNINGNIGHTIAND",
+    "COOLCOLDWARMHOTV",
+];
+
+var str = "<div><h1>";
+
+for (var x = 0; x < 16; x++) {
+    for (var y = 0; y < 16; y++) {
+        str += '<span class="block' + ' r' + x.toString(10) + 'c' + y.toString(10) + '"' + '>' + wordMatrix[x][y] + '</span>';
+    }
+    str += '<br>';
+}
+str += '</h1></div>'
+
+var div = document.getElementById('matrix');
+div.innerHTML = str;
+
+addToFrame(2, 0, 2);
+addToFrame(2, 4, 2);
+addToFrame(4, 8, 5);
+addToFrame(10, 7, 6);
+
+
+function addToFrame(row, col, length) {
+    var styles = '';
+    for (var x = col; x < (col + length); x++) {
+        styles += `.r` + row.toString(10) + `c` + x.toString(10) + ` {
+        color: rgb(80, 80, 255);
+      }`
+    }
+    var styleSheet = document.createElement("style")
+    styleSheet.innerText += styles
+    document.head.appendChild(styleSheet)
+}
